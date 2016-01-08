b3cd5ecc247947d8efae124a2dd9f4c3fabe0607
Zach Brown <xzachx@alum.wpi.edu>
2016-01-08 15:52:24 -0500

    Added dictionary

--- RSS Duplicate Feed Grouper/rss_grouper.py
+++ RSS Duplicate Feed Grouper/rss_grouper.py
@@ -4,13 +4,32 @@
 
 import csv
 import feedparser
+from urlparse import urlparse
 
 with open('feed_list.tsv', 'rb') as input:
     reader = csv.reader(input, delimiter='\t')
     next(reader, None)
-    urls = []
+    urls = dict()
     for line in reader:
-        urls += line[1:]
-    
-parsed_url = feedparser.parse(urls[0])
-print parsed_url
+        url = line[1:][0]
+        parsed_url = urlparse(url)
+        base_url = '{url.scheme}://{url.netloc}/'.format(url=parsed_url)
+
+        if base_url not in urls:
+            urls[base_url] = {url}
+            
+        else:
+            urls[base_url].add(url)
+
+# Determine anagram with the most instances
+most_urls = max(urls, key=lambda k: len(urls[k]))
+
+# Determine number of instances for the most used anagram
+most_urls_count = len(url[most_urls])
+ 
+#for url in urls:    
+#    parsed_url = feedparser.parse(url)
+#    try:
+#        print parsed_url['feed']['title']
+#    except KeyError:
+#        pass
