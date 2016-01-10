a7784488bcabf7746d88d3863270096ffe51fc74
Zach Brown <xzachx@alum.wpi.edu>
2016-01-10 18:47:56 -0500

    Version 1.0

--- RSS Duplicate Feed Grouper/rss_grouper.py
+++ RSS Duplicate Feed Grouper/rss_grouper.py
@@ -2,34 +2,50 @@
 # xzachx@alum.wpi.edu
 # rss_grouper.py
 
+# Import libraries for reading/writing tsv files and parsing urls
 import csv
-import feedparser
 from urlparse import urlparse
 
+# Read in feed list tsv file
 with open('feed_list.tsv', 'rb') as input:
     reader = csv.reader(input, delimiter='\t')
+    
+    # Skip header row
     next(reader, None)
+    
+    # Initialize dictionary to read feeds into
     urls = dict()
+    
+    # Read feeds into urls dictionary
     for line in reader:
+        
+        # Strip off feed id from line
         url = line[1:][0]
+        
+        # Strip off parameters to get the base url
         parsed_url = urlparse(url)
-        base_url = '{url.scheme}://{url.netloc}/'.format(url=parsed_url)
+        base_url = '{url.scheme}://{url.netloc}{url.path}'.format(url=parsed_url)
 
+        # Add key/value entry for the base url in the dictionary if it doesn't exist
         if base_url not in urls:
             urls[base_url] = {url}
-            
+        
+        # Otherwise, append the url for the list in the value for the base url
         else:
             urls[base_url].add(url)
 
-# Determine anagram with the most instances
-most_urls = max(urls, key=lambda k: len(urls[k]))
-
-# Determine number of instances for the most used anagram
-most_urls_count = len(url[most_urls])
- 
-#for url in urls:    
-#    parsed_url = feedparser.parse(url)
-#    try:
-#        print parsed_url['feed']['title']
-#    except KeyError:
-#        pass
+# Write the output to a new tsv file
+with open('feed_groupings.tsv', 'wb') as output:
+    
+    # Write the header row
+    output.write('Base_Url\tFeed\n')
+    
+    # Iterate through each key in the dictionary
+    for url in urls:
+        
+        # Iterate through each list entry in the value for the given key
+        for feed in urls[url]:
+            
+            # Write the base url and feed url to the output file
+            output.write('%s\t%s\n' % (url, feed))
+           
